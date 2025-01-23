f2b9535a6636d7cb91dd8c8d86286153799d8e7a
youyu <129youyu@gmail.com>
2025-01-23 22:56:49 +0800

    新增insert檔案 檔案上傳成功時搬移檔案 並更名 將post存入資料庫 回到標題頁面

--- api/insert.php
+++ api/insert.php
@@ -2,7 +2,18 @@
 
 include_once "db.php";
 
-$_POST['img'];
-$_POST['text'];
+// $_POST['img'];
+// $_POST['text'];
+// $_POST['sh'];
+
+if(!empty($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_FILES['img']['name']);
+    $_POST['img']=$_FILES['img']['name'];
+    
+}
+
+$Title->save($_POST);
+
+to("../admin.php?do=title");
 
 ?>
