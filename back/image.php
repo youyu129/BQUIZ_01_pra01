fbc614a749faefef372ad9a4d076a797aec7f310
youyu <129youyu@gmail.com>
2025-01-23 22:00:15 +0800

    複製title為ad mv image並修改文字

--- back/image.php
+++ back/image.php
@@ -31,7 +31,7 @@
                     <tr>
                         <td width="200px"><input type="button"
                                 onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/<?=$do;?>.php&#39;)"
-                                value="新增動態文字廣告"></td>
+                                value="新增網站標題圖片"></td>
                         <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
                         </td>
                     </tr>
