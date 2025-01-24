3692af2781f6b3b2a318b2cfa2128866b250fd59
youyu <129youyu@gmail.com>
2025-01-24 16:56:28 +0800

    複製api的edit為edit_ad並修改$Ad 單筆顯示修改為多筆顯示

---
+++ api/edit_ad.php
@@ -0,0 +1,17 @@
+<?php
+include_once "db.php";
+
+if(isset($_POST['id'])){
+    foreach($_POST['id'] as $idx => $id){
+        if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+            $Title->del($id);
+        }else{
+            $row=$Ad->find($id);
+            $row['text']=$_POST['text'][$idx];
+            $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+            $Ad->save($row);
+        }
+    }
+}
+to("../admin.php?do=ad")
+?>
