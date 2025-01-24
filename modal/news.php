c7516162f1d6cb40b6c487e9d6f86c9257527842
youyu <129youyu@gmail.com>
2025-01-25 00:02:33 +0800

    新增modal的news彈出視窗畫面

---
+++ modal/news.php
@@ -0,0 +1,17 @@
+<h3 class="cent">新增最新消息資料</h3>
+<hr>
+<form action="api/insert.php" method="post" enctype="multipart/form-data">
+    <table>
+        <tr>
+            <td>最新消息資料：</td>
+            <td>
+                <input type="text" name="text" id="text">
+            </td>
+        </tr>
+    </table>
+    <div class="cent">
+        <input type="hidden" name="table" value="ad">
+        <input type="submit" value="新增">
+        <input type="reset" value="重置">
+    </div>
+</form>
