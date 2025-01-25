dd8fae34108f8bb1b16ef27faf76e284c0a748c2
youyu <129youyu@gmail.com>
2025-01-25 13:42:29 +0800

    修改edit的menu的顯示設定

--- api/edit.php
+++ api/edit.php
@@ -25,6 +25,7 @@ if(isset($_POST['id'])){
                 case 'menu':
                     $row['text']=$_POST['text'][$idx];
                     $row['href']=$_POST['href'][$idx];
+                    $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
                     break;
                             
                 default:
