c0b30f75cffb4437e5ceb323646effcc60263284
youyu <129youyu@gmail.com>
2025-01-25 12:33:23 +0800

    新增後台admin的畫面

---
+++ back/admin.php
@@ -0,0 +1,63 @@
+<div class="di"
+    style="height:540px; border:#999 1px solid; width:76.5%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
+    <!--正中央-->
+    <table width="100%">
+        <tbody>
+            <tr>
+                <td style="width:70%;font-weight:800; border:#333 1px solid; border-radius:3px;" class="cent"><a
+                        href="?do=admin" style="color:#000; text-decoration:none;">後台管理區</a>
+                </td>
+                <td><button onclick="document.cookie=&#39;user=&#39;;location.replace(&#39;?&#39;)"
+                        style="width:99%; margin-right:2px; height:50px;">管理登出</button></td>
+            </tr>
+        </tbody>
+    </table>
+    <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+        <p class="t cent botli">管理者帳號管理</p>
+        <form method="post" action="./api/edit.php">
+            <table width="100%" class="cent">
+                <tbody>
+                    <tr class="yel">
+                        <td width="45%">帳號</td>
+                        <td width="45%">密碼</td>
+                        <td width="10%">刪除</td>
+                        <td></td>
+                    </tr>
+                    <?php
+                    $rows=$Admin->all();
+                    foreach($rows as $row):
+                    ?>
+                    <tr>
+                        <td>
+                            <input type="text" name="acc[]" style="width:80%" value="<?=$row['acc'];?>">
+                        </td>
+                        <td>
+                            <input type="password" name="pw[]" style="width:80%" value="<?=$row['pw'];?>">
+                        </td>
+                        <td>
+                            <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                        </td>
+
+                        <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                    </tr>
+                    <?php endforeach;?>
+                </tbody>
+            </table>
+            <table style="margin-top:40px; width:70%;">
+                <tbody>
+                    <tr>
+                        <td width="200px"><input type="button"
+                                onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/<?=$do;?>.php&#39;)"
+                                value="新增管理者帳號"></td>
+                        <td class="cent">
+                            <input type="hidden" name="table" value="<?=$do;?>">
+                            <input type="submit" value="修改確定">
+                            <input type="reset" value="重置">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </form>
+    </div>
+</div>
