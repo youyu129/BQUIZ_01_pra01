b48deb4f82e2a964d5f7f95eb413d3280f121963
youyu <129youyu@gmail.com>
2025-01-26 17:47:32 +0800

    將api的update檔案改為可變變數 可共用

--- api/update.php
+++ api/update.php
@@ -1,13 +1,18 @@
 <?php
 include_once  "db.php";
 
+$table=$_POST['table'];
+$db=ucfirst($table);
+
 if(isset($_FILES['img']['tmp_name'])){
     move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_FILES['img']['name']);  
 
-    $row=$Title->find($_POST['id']);
+    $row=$$db->find($_POST['id']);
     $row['img']=$_FILES['img']['name'];
-    $Title->save($row);
+    $$db->save($row);
 }
 
-to("../admin.php?do=title");
+unset($_POST['table']);
+
+to("../admin.php?do=$table");
 ?>
