c5934b8258be8719e4e8e1af86b8674d55221665
youyu <129youyu@gmail.com>
2025-01-29 19:54:21 +0800

    修改api的submenu裡面的POST['del']

--- api/submenu.php
+++ api/submenu.php
@@ -4,7 +4,7 @@ include_once "db.php";
 
 if(isset($_POST['id'])){
     foreach($_POST['id'] as $idx => $id){
-        if(isset($_POST['del']) && in_array($id,$_POST['id'])){
+        if(isset($_POST['del']) && in_array($id,$_POST['del'])){
             $Menu->del($id);
         }else{
             $row=$Menu->find($id);
